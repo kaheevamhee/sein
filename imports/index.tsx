b4ec0137eb7afbabb2af51f810187a3782e1f66b
vamvamhee <gahee9370@gmail.com>
2026-08-12 17:02:38 +0900

    Add files via upload

---
+++ imports/index.tsx
@@ -0,0 +1,329 @@
+import svgPaths from "./svg-hotst6q1xi";
+import imgGettyImages1163769212 from "./a96a5d4c0384a108666891974412e5a97c0a85f6.png";
+import imgG81 from "./3aa643c6e4688b319f522ca4aad06ae8b418694e.png";
+import { imgGettyImages1163769211 } from "./svg-xm0cv";
+
+function Image() {
+  return (
+    <div className="absolute contents left-0 top-0" data-name="image">
+      <div className="absolute flex h-[1316px] items-center justify-center left-[-11px] top-[-91px] w-[1975px]">
+        <div className="-scale-y-100 flex-none rotate-180">
+          <div className="h-[1316px] mask-alpha mask-intersect mask-no-clip mask-no-repeat mask-position-[11px_91px] mask-size-[1920px_1080px] relative w-[1975px]" style={{ maskImage: `url("${imgGettyImages1163769211}")` }} data-name="GettyImages-116376921 1">
+            <div aria-hidden className="absolute inset-0 pointer-events-none">
+              <img alt="" className="absolute max-w-none object-cover size-full" src={imgGettyImages1163769212} />
+              <div className="absolute bg-[rgba(0,0,0,0.4)] inset-0" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="absolute h-[1093px] left-[-19px] mask-alpha mask-intersect mask-no-clip mask-no-repeat mask-position-[19px_7px] mask-size-[1920px_1080px] top-[-7px] w-[1957px]" style={{ maskImage: `url("${imgGettyImages1163769211}")` }} data-name="G8+소아_일상_게임 1">
+        <div aria-hidden className="absolute inset-0 pointer-events-none">
+          <img alt="" className="absolute max-w-none object-cover size-full" src={imgG81} />
+          <div className="absolute bg-[rgba(0,0,0,0.5)] inset-0" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Icon() {
+  return (
+    <div className="absolute left-[260px] size-[16px] top-[742px]" data-name="Icon">
+      <svg className="absolute block inset-0 size-full" fill="none" height="16" preserveAspectRatio="none" viewBox="0 0 16 16" width="16">
+        <g id="Icon">
+          <path d={svgPaths.p11ad2f00} id="Vector" stroke="#20C3A8" strokeLinecap="round" strokeLinejoin="round" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Text() {
+  return (
+    <div className="absolute content-stretch flex flex-col items-start left-[281px] top-[740px]" data-name="Text">
+      <p className="[word-break:break-word] font-['Pretendard:Medium',sans-serif] leading-[20px] not-italic relative shrink-0 text-[14px] text-[rgba(255,255,255,0.7)] tracking-[-0.35px] whitespace-nowrap">인증된 의료 기기와 신뢰할 수 있는 글로벌 파트너들이 안전한 플랫폼을 보장합니다.</p>
+    </div>
+  );
+}
+
+function Group5() {
+  return (
+    <div className="absolute contents left-[260px] top-[740px]">
+      <Icon />
+      <Text />
+    </div>
+  );
+}
+
+function Frame() {
+  return (
+    <div className="absolute bg-[#20c3a8] content-stretch flex h-[56px] items-center justify-center left-[260px] rounded-[6px] top-[655px] w-[170px]">
+      <div className="[word-break:break-word] flex flex-col font-['Pretendard:Medium',sans-serif] justify-center leading-[0] not-italic relative shrink-0 text-[16px] text-center text-white whitespace-nowrap">
+        <p className="leading-[normal]">Demo 요청</p>
+      </div>
+    </div>
+  );
+}
+
+function Frame1() {
+  return (
+    <div className="absolute content-stretch flex h-[56px] items-center justify-center left-[445px] rounded-[6px] top-[655px] w-[170px]">
+      <div aria-hidden className="absolute border border-solid border-white inset-0 pointer-events-none rounded-[6px]" />
+      <div className="[word-break:break-word] flex flex-col font-['Pretendard:Medium',sans-serif] justify-center leading-[0] not-italic relative shrink-0 text-[16px] text-center text-white whitespace-nowrap">
+        <p className="leading-[normal]">플랫폼 살펴보기</p>
+      </div>
+    </div>
+  );
+}
+
+function Group1() {
+  return (
+    <div className="absolute contents left-[260px] top-[655px]">
+      <Frame />
+      <Frame1 />
+    </div>
+  );
+}
+
+function Group2() {
+  return (
+    <div className="absolute contents left-[260px] top-[304px]">
+      <div className="[word-break:break-word] absolute font-['Pretendard:SemiBold',sans-serif] h-[235px] leading-[0] left-[260px] not-italic text-[70px] text-white top-[304px] w-[663px] whitespace-pre-wrap">
+        <p className="leading-[1.1] mb-0">{`인슐린에서의 자유 `}</p>
+        <p className="leading-[1.1] mb-0">삶 속의 기쁨</p>
+        <p className="leading-[1.1]">Glumics와 함께</p>
+      </div>
+      <div className="-translate-y-1/2 [word-break:break-word] absolute flex flex-col font-['Pretendard:Medium',sans-serif] justify-center leading-[0] left-[260px] not-italic text-[18px] text-white top-[582px] w-[504px] whitespace-pre-wrap">
+        <p className="leading-[1.5] mb-0">{`Glumics는 기기, 데이터, 케어를 하나로 연결하여 `}</p>
+        <p className="leading-[1.5]">매일 더 나은 결과를 만들어냅니다.</p>
+      </div>
+      <Group5 />
+      <Group1 />
+    </div>
+  );
+}
+
+function Group3() {
+  return (
+    <div className="[word-break:break-word] absolute contents font-['Pretendard:SemiBold',sans-serif] leading-[0] left-[758px] not-italic text-[16px] top-[1005px] whitespace-nowrap">
+      <div className="-translate-y-1/2 absolute flex flex-col justify-center left-[870px] text-[rgba(255,255,255,0.55)] top-[1015px]">
+        <p className="leading-[20px]">Patients</p>
+      </div>
+      <div className="-translate-y-1/2 absolute flex flex-col justify-center left-[758px] text-white top-[1015px]">
+        <p className="leading-[20px]">Home</p>
+      </div>
+      <div className="-translate-y-1/2 absolute flex flex-col justify-center left-[985px] text-[rgba(255,255,255,0.55)] top-[1015px]">
+        <p className="leading-[20px]">Providers</p>
+      </div>
+      <div className="-translate-y-1/2 absolute flex flex-col justify-center left-[1108px] text-[rgba(255,255,255,0.55)] top-[1015px]">
+        <p className="leading-[20px]">Partners</p>
+      </div>
+    </div>
+  );
+}
+
+function Group4() {
+  return (
+    <div className="-translate-x-1/2 absolute contents left-1/2 top-[983px]">
+      <div className="-translate-x-1/2 absolute backdrop-blur-[10px] bg-[rgba(255,255,255,0.15)] h-[64px] left-1/2 rounded-[14px] top-[983px] w-[504px]" />
+      <div className="-translate-x-1/2 absolute backdrop-blur-[10px] bg-[#20c3a8] h-[40px] left-[calc(50%-180px)] rounded-[10px] top-[995px] w-[120px]" />
+      <Group3 />
+    </div>
+  );
+}
+
+function DeviceDesktop() {
+  return (
+    <div className="relative shrink-0 size-[24px]" data-name="device-desktop">
+      <svg className="absolute block inset-0 size-full" fill="none" height="24" preserveAspectRatio="none" viewBox="0 0 24 24" width="24">
+        <g id="device-desktop">
+          <path d={svgPaths.p3b943500} id="Vector" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Icon2() {
+  return (
+    <div className="aspect-[80/80] bg-black content-stretch flex items-center justify-center relative rounded-[100px] shrink-0 w-full" data-name="icon">
+      <div aria-hidden className="absolute border border-[rgba(255,255,255,0.2)] border-solid inset-0 pointer-events-none rounded-[100px]" />
+      <DeviceDesktop />
+    </div>
+  );
+}
+
+function Messages() {
+  return (
+    <div className="relative shrink-0 size-[24px]" data-name="messages">
+      <svg className="absolute block inset-0 size-full" fill="none" height="24" preserveAspectRatio="none" viewBox="0 0 24 24" width="24">
+        <g id="messages">
+          <path d={svgPaths.p5d87200} id="Vector" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Icon3() {
+  return (
+    <div className="aspect-[80/80] bg-black content-stretch flex items-center justify-center relative rounded-[100px] shrink-0 w-full" data-name="icon">
+      <div aria-hidden className="absolute border border-[rgba(255,255,255,0.2)] border-solid inset-0 pointer-events-none rounded-[100px]" />
+      <Messages />
+    </div>
+  );
+}
+
+function ArrowNarrowUp() {
+  return (
+    <div className="relative shrink-0 size-[24px]" data-name="arrow-narrow-up">
+      <svg className="absolute block inset-0 size-full" fill="none" height="24" preserveAspectRatio="none" viewBox="0 0 24 24" width="24">
+        <g id="arrow-narrow-up">
+          <path d="M12 5V19M8 9L12 5L16 9" id="Vector" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Icon4() {
+  return (
+    <div className="aspect-[80/80] bg-black content-stretch flex items-center justify-center relative rounded-[100px] shrink-0 w-full" data-name="icon">
+      <div aria-hidden className="absolute border border-[rgba(255,255,255,0.2)] border-solid inset-0 pointer-events-none rounded-[100px]" />
+      <ArrowNarrowUp />
+    </div>
+  );
+}
+
+function Icon1() {
+  return (
+    <div className="absolute content-stretch flex flex-col gap-[17px] items-start left-[1822px] top-[821px] w-[64px]" data-name="icon">
+      <Icon2 />
+      <Icon3 />
+      <Icon4 />
+    </div>
+  );
+}
+
+function Group() {
+  return (
+    <div className="h-[28.001px] relative shrink-0 w-[145.84px]" data-name="Group">
+      <svg className="absolute block inset-0 size-full" fill="none" height="28" preserveAspectRatio="none" viewBox="0 0 145.839 28" width="145.839">
+        <g id="Group">
+          <path d={svgPaths.p2fbca00} fill="#20C3A8" id="Vector" />
+          <path d={svgPaths.p2392d780} fill="#20C3A8" id="Vector_2" />
+          <path d={svgPaths.pacc800} fill="#20C3A8" id="Vector_3" />
+          <path d={svgPaths.paf0f800} fill="#20C3A8" id="Vector_4" />
+          <path d={svgPaths.p1c50a9f2} fill="#20C3A8" id="Vector_5" />
+          <path d={svgPaths.p204e1e80} fill="#20C3A8" id="Vector_6" />
+          <path d={svgPaths.p390b1fb0} fill="#20C3A8" id="Vector_7" />
+          <path d={svgPaths.p32c2db00} fill="#20C3A8" id="Vector_8" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Component() {
+  return (
+    <div className="[word-break:break-word] content-stretch flex font-['Pretendard:SemiBold',sans-serif] gap-[40px] items-center leading-[0] not-italic relative shrink-0 text-[18px] text-white whitespace-nowrap" data-name="메뉴">
+      <div className="flex flex-col justify-center relative shrink-0">
+        <p className="leading-[1.4]">Digital care</p>
+      </div>
+      <div className="flex flex-col justify-center relative shrink-0">
+        <p className="leading-[1.4]">Product</p>
+      </div>
+      <div className="flex flex-col justify-center relative shrink-0">
+        <p className="leading-[1.4]">Right for me?</p>
+      </div>
+      <div className="flex flex-col justify-center relative shrink-0">
+        <p className="leading-[1.4]">Support</p>
+      </div>
+      <div className="flex flex-col justify-center relative shrink-0">
+        <p className="leading-[1.4]">Community</p>
+      </div>
+    </div>
+  );
+}
+
+function Menu1() {
+  return (
+    <div className="content-stretch flex gap-[80px] items-center relative shrink-0" data-name="menu">
+      <Group />
+      <Component />
+    </div>
+  );
+}
+
+function Frame2() {
+  return (
+    <div className="content-stretch flex gap-[10px] items-center relative shrink-0">
+      <div className="[word-break:break-word] flex flex-col font-['Pretendard:Regular',sans-serif] justify-center leading-[0] not-italic relative shrink-0 text-[16px] text-white whitespace-nowrap">
+        <p className="leading-[1.4]">For Partners</p>
+      </div>
+      <div className="flex h-[14px] items-center justify-center relative shrink-0 w-0">
+        <div className="flex-none rotate-90">
+          <div className="h-0 relative w-[14px]">
+            <div className="absolute inset-[-1px_0_0_0]">
+              <svg className="block size-full" fill="none" height="1" preserveAspectRatio="none" viewBox="0 0 14 1" width="14">
+                <line id="Line 21" opacity="0.3" stroke="white" x2="14" y1="0.5" y2="0.5" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="[word-break:break-word] flex flex-col font-['Pretendard:Regular',sans-serif] justify-center leading-[0] not-italic relative shrink-0 text-[16px] text-white whitespace-nowrap">
+        <p className="leading-[1.4]">For healthcare Providers</p>
+      </div>
+    </div>
+  );
+}
+
+function Btn() {
+  return (
+    <div className="bg-[#20c3a8] content-stretch flex h-[42px] items-center justify-center px-[20px] relative rounded-[6px] shrink-0" data-name="btn">
+      <div className="[word-break:break-word] flex flex-col font-['Pretendard:SemiBold',sans-serif] justify-center leading-[0] not-italic relative shrink-0 text-[16px] text-white whitespace-nowrap">
+        <p className="leading-none">{`Get started `}</p>
+      </div>
+    </div>
+  );
+}
+
+function Util() {
+  return (
+    <div className="content-stretch flex gap-[20px] items-center relative shrink-0" data-name="util">
+      <Frame2 />
+      <Btn />
+    </div>
+  );
+}
+
+function Menu() {
+  return (
+    <div className="content-stretch flex items-center justify-between relative shrink-0 w-[1400px]" data-name="menu">
+      <Menu1 />
+      <Util />
+    </div>
+  );
+}
+
+function Gnb() {
+  return (
+    <div className="absolute content-stretch flex flex-col h-[90px] items-center justify-center left-0 px-[260px] top-0 w-[1920px]" data-name="gnb">
+      <Menu />
+    </div>
+  );
+}
+
+export default function Component1() {
+  return (
+    <div className="relative size-full" data-name="7">
+      <div className="absolute bg-black border-black border-solid border-t h-[1080px] left-0 top-0 w-[1920px]" />
+      <Image />
+      <Group2 />
+      <Group4 />
+      <Icon1 />
+      <Gnb />
+    </div>
+  );
+}
